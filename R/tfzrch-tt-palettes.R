4844e89ce3cbb7764360e5596d2043488855dc97
Tom Fowler <41972466+tfzrch@users.noreply.github.com>
2020-08-23 13:16:42 +0100

    Added script for defining colour palettes for custom ggplot2 themes

---
+++ R/tfzrch-tt-palettes.R
@@ -0,0 +1,11 @@
+# Palettes -----------------------------------
+tfzrch_tt_palettes <- list()
+
+# * Desert ----
+tfzrch_tt_palettes[["desert"]] <- c(
+  "dark red" = "#cc4f39",
+  "light orange" = "#f09b6c", 
+  "teal" =  "#ffebd6", 
+  "light blue" = "#bbdef0", 
+  "cream" = "#00a6a6"
+)
