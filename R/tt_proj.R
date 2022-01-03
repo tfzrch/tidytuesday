6959614e52d5cd7f89de7db276f99b625cce5f9e
Tom Fowler <41972466+tfzrch@users.noreply.github.com>
2022-01-03 10:51:39 +0000

    Add function for creating consistently named git branches for tt projects

--- R/tt_proj.R
+++ R/tt_proj.R
@@ -9,7 +9,6 @@
 #' 
 NULL
 
-
 #' @describeIn ttproj Create new TidyTuesday directory
 tt_dir_create <- function(iso, add.readme = FALSE, readme.ext = "md") {
   
@@ -30,4 +29,29 @@ tt_dir_create <- function(iso, add.readme = FALSE, readme.ext = "md") {
   if (add.readme == TRUE) {
     file.create(here::here(year, iso, paste0("README", ".", readme.ext)))
   }
+}
+
+tt_git_branch_new <- function(iso, prefix = "tt-") {
+  
+  # New and old branch names
+  new_branch_name <- paste0(prefix, iso)
+  current_branch <- gert::git_branch()
+  
+  # Compose message for prompt
+  msg_glue <- paste(
+    "Create and checkout new branch with name `{new_branch_name}`",
+    "on current branch `{current_branch}`?"
+  )
+  
+  msg <- glue::glue(msg_glue)
+  
+  # Prompt user for confirmation and create branch if TRUE
+  if (ui_yeah(msg)) {
+    gert::git_branch_create(new_branch_name)
+    rlang::inform(glue::glue("{new_branch_name} created and checked out"))
+  } else {
+    rlang::inform("New branch abandoned.")
+  }
+  
+  invisible()
 }
